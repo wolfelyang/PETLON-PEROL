47aeb89b6ec449f3b4bb1d044df03c6ad6990cb7
LYU <wolfgang.yang@gmail.com>
2018-08-16 18:01:45 -0500

    initial commit

---
+++ experiment-singleplan-rlearning.py
@@ -0,0 +1,289 @@
+import os
+import numpy as np
+import time, random, math
+import matplotlib.pyplot as plt
+from taxi import TaxiEnv
+import planner
+import time
+
+
+
+def generateplan():
+    clingopath = "clingcon"
+    initial = "initial.lp"
+    goal = "goal.lp"
+    planning = "taxi-bk.lp"
+    qvalue = "q.lp"
+    return planner.compute_plan(clingopath = clingopath, initial = initial, goal=goal, planning=planning, qvalue=qvalue, printout=True)
+
+
+def getAction(state):
+    if random.random() < BETA:
+        r = getRLAction(state)
+        return (False,r)
+    else:
+        r = getSymbolicAction(state)
+        return (True,r)
+
+def getRLAction(state):
+    if random.random() < EPSILON:
+        return env.action_space.sample()
+    else:
+        return np.argmax(q_table[state])
+
+def getSymbolicAction(plantrace,i):
+    unit = plantrace[i]
+    action = unit[1]
+    if action.startswith("move(n)"):
+        return 1
+    if action.startswith("move(s)"):
+        return 0
+    if action.startswith("move(e)"):
+        return 2
+    if action.startswith("move(w)"):
+        return 3
+    if action.startswith("pickup"):
+        return 4
+    if action.startswith("dropoff"):
+        return 5
+
+def getActionName(i):
+    if i == 1:
+        return "move(n)"
+    if i == 0:
+        return "move(s)"
+    if i == 2:
+        return "move(e)"
+    if i == 3:
+        return "move(w)"
+    if i == 4:
+        return "pickup(g)"
+    if i == 5:
+        return "dropoff(g)"
+
+def generate_initial_state(env):
+    inputfile = open("initial.lp","w")
+#    env.render()
+    taxirow, taxicol, passidx, destidx = env.decode(env.s)
+#    print "taxi=",(taxirow,taxicol),"guestloc=",env.locs[passidx],"destloc=",env.locs[destidx]
+    inputfile.write("taxiloc("+str(taxirow)+","+str(taxicol)+").\n")
+    inputfile.write("guestloc("+str(env.locs[passidx][0])+","+str(env.locs[passidx][1])+").\n")
+    inputfile.write("destloc("+str(env.locs[destidx][0])+","+str(env.locs[destidx][1])+").\n")
+    inputfile.close()
+    return passidx, destidx
+
+def generate_lp_key(env,state,passidx,destidx,action):
+    taxirow, taxicol, _, _ = env.decode(state)
+    guestloc = env.locs[passidx]
+    destloc = env.locs[destidx]
+    actionname = getActionName(action)
+    return (taxirow,taxicol,guestloc[0],guestloc[1],destloc[0],destloc[1],actionname)
+
+def generate_qvalue_lp(q_table_lp):
+#    print "output qvalues"
+    qfile = open("q.lp","w")
+    for key in q_table_lp.keys():
+        qrule = "q(("+str(key[0])+","+str(key[1])+","+str(key[2])+","+str(key[3])+","+str(key[4])+","+str(key[5])+"),"+key[6]+","+str(int(math.floor(q_table_lp[key])))+").\n"
+        qfile.write(qrule)
+    qfile.close()
+
+
+
+def generate_goal_file(planquality):
+#    print "output new goal file"
+    goalfile = open("goal.lp","w")
+    goalfile.write("#program check(k).\n")
+    goalfile.write(":- not finished(g,k), query(k).\n")
+    goalfile.write(":- &sum{cost(k)} <= "+str(planquality)+",query(k).")
+    goalfile.close()
+
+def restore_goal_file():
+#    print "restore goal file"
+    goalfile = open("goal.lp","w")
+    goalfile.write("#program check(k).\n")
+    goalfile.write(":- not finished(g,k), query(k).\n")
+    goalfile.close()
+
+
+def pause():
+    os.system('read -s -n 1 -p "Press any key to continue...\n"')
+
+def throwdice(threshold):
+    rand =  random.uniform(0,1)
+    if rand < threshold:
+        return True
+    else:
+        return False
+
+def calculateplanquality(env,ro_table,stateaction):
+    planquality = 0
+    for (state,action) in stateaction:
+        planquality += int(math.floor(ro_table[state,action]))
+ #       taxirow,taxicol,_,_ = env.decode(state)
+ #       actionname = getActionName(action)
+ #       print "ro(pos(",taxirow,taxicol,")",actionname,")=",ro_table[state,action],int(math.floor(ro_table[state,action])),planquality
+ #   print "plan quality:",planquality
+#    pause()
+    return planquality
+
+def generate_rovalue_from_table(env,q_table_lp,ro_table):
+#    print "output qvalues"
+    qt = set(q_table_lp)
+    qfile = open("q.lp","w")
+    for (state,action) in qt:
+        taxirow,taxicol, passidx, _ = env.decode(state)
+        actionname = getActionName(action)
+    #    comment = "% taxi:"+str(taxirow)+","+str(taxicol)+",passenger:"+str(passidx)+"\n"
+        qrule = "q(("+str(taxirow)+","+str(taxicol)+","+str(passidx)+"),"+actionname+","+str(int(math.floor(ro_table[state,action])))+").\n"
+        qfile.write(qrule)
+    qfile.close()
+
+def generate_qvalue_from_table(env,q_table_lp,ro_table):
+#    print "output qvalues"
+    qt = set(q_table_lp)
+    qfile = open("other.lp","w")
+    for (state,action) in qt:
+        taxirow,taxicol, passidx, _ = env.decode(state)
+        actionname = getActionName(action)
+    #    comment = "% taxi:"+str(taxirow)+","+str(taxicol)+",passenger:"+str(passidx)+"\n"
+        qrule = "q(("+str(taxirow)+","+str(taxicol)+","+str(passidx)+"),"+actionname+","+str(ro_table[state,action])+").\n"
+        qfile.write(qrule)
+    qfile.close()
+
+
+if __name__=='__main__':
+
+
+
+    env = TaxiEnv()
+
+    nA = env.nA
+    nS = env.nS
+
+    # Parameters
+    LEARNING_RATE_Q = 1
+    LEARNING_RATE_R = 1
+    DISCOUNT = 0.001
+    EPSILON = 0.01
+    BETA = 0.3
+
+    # Q and rewards
+#    q_table = np.zeros((nS, nA))
+#    q_table_lp = {}
+
+    runs = 1 # multiple runs
+    episodes = 1000
+    reward_epi = np.zeros((runs, episodes))
+
+    # multiple runs
+    for run in xrange(runs):
+        q_table = np.zeros((nS, nA))
+        ro_table = np.zeros((nS, nA))
+        q_table_lp = []
+
+        other_trained_data = {}
+        restore_goal_file()
+        converged = False
+    # Episodes
+        explore = True
+        plantrace = []
+        for episode in xrange(episodes):
+            # Refresh state
+
+            stateaction = []
+            rewardpicked = False
+            if episode % 50 == 0:
+                print "Run",run,",Episode",episode
+            planquality = 0
+            taxirow = 0#0
+            taxicol = 4#4
+            passidx = 0#1
+            destidx = 1#3
+            env.s = env.encode(taxirow,taxicol,passidx,destidx)
+            state = env.s
+            passidx, destidx = generate_initial_state(env)
+         #   generate_qvalue_lp(env,q_table_lp)
+            generate_rovalue_from_table(env,q_table_lp,q_table)
+            generate_qvalue_from_table(env,q_table_lp,true_q_table)
+            if explore:
+                print "generate new plan..."
+                oldplan = plantrace
+                plantrace = generateplan()
+                if plantrace == None:
+                    print "Run",run
+                    print "No plan found at Episode",episode
+                    converged = True
+                    break
+                    plantrace = oldplan
+            if not explore:
+                print "continue executing previous plan..."
+            done = False
+            total_reward = 0
+            # Run episode
+            for i in xrange(len(plantrace)-1):
+                action = getSymbolicAction(plantrace,i)
+                if action != None:
+                    stateaction.append((state,action))
+                    state_next, reward, done, info = env.step(action)
+                    taxirow,taxicol,passidx,destidx = env.decode(state_next)
+                    if ((taxirow,taxicol) == (4,4)) and (not rewardpicked):
+                        reward = 10
+                        rewardpicked = True
+                elif action == None:
+                    print "action not found",action,i
+                    exit()
+                total_reward += reward
+            # R learning
+            #    env.render()
+            #    print taxirow,taxicol,passidx,destidx
+            #    pause()
+            #    true_q_table[state, action] += LEARNING_RATE_Q * (reward + DISCOUNT * max(true_q_table[state_next, :]) - q_table[state, action])
+
+                q_table[state, action] += 0.1 * (reward - ro_table[state,action] + max(q_table[state_next, :]) - q_table[state, action])
+                ro_table[state, action] += 0.5 * (reward + max(q_table[state_next, :]) - max(q_table[state, :])- ro_table[state,action])
+
+            # Q learning
+            #    q_table[state, action] += LEARNING_RATE_Q * (reward + DISCOUNT * max(q_table[state_next, :]) - q_table[state, action])
+            #    ro_table[state, action] += LEARNING_RATE_R * (reward + max(q_table[state_next, :]) - max(q_table[state, :])- ro_table[state,action])
+
+                if (state,action) not in q_table_lp:
+                    q_table_lp.append((state,action))
+                r,c,_,_ = env.decode(state)
+                an = getActionName(action)
+
+            #    planquality += int(math.floor(ro_table[state,action]))
+            #    print "pos(",r,c,")",an,ro_table[state,action],int(math.floor(ro_table[state,action])),planquality
+
+            #    print "true_q_table",r,c,an,true_q_table[state,action]
+                if done:
+                    break
+                state = state_next
+            # pause()
+
+            planquality = calculateplanquality(env,q_table,stateaction)
+            reward_epi[run, episode] = total_reward
+            print "run",run,"episode",episode,"total reward",total_reward,"plan quality",planquality
+        #    pause()
+        #    eps = min(0.2,1-episode/episodes)
+        #    explore = True
+        #    eps = max(0.2,1.5/total_reward)
+            eps = 0.2
+            explore = throwdice(eps) and not converged
+            if explore:
+                generate_goal_file(planquality)
+
+
+
+    env.close()
+
+    #plot
+    plt.figure(figsize=(15, 10))
+    plt.xlabel('Episode', fontsize=20)
+    plt.xlim(-4, episodes+4)
+    plt.ylabel('Average Reward', fontsize=20)
+    mean_reward = np.mean(reward_epi, axis=0)
+    std_reward = np.std(reward_epi, axis=0)
+    plt.errorbar(xrange(episodes), mean_reward, color='b', linewidth=1.5)
+    plt.fill_between(xrange(episodes), (mean_reward - std_reward), (mean_reward + std_reward), color='b', alpha=0.3)
+    plt.savefig('cumulative_reward_curve_symbolic.png')
+    # plt.show()
---
+++ navigation.asp
@@ -0,0 +1,101 @@
+#include <incmode>.
+
+#const istop  = "SAT".
+#const imin   = 1.
+#const imax  = 50.
+
+#program base.
+cost(0,0).
+
+heuristics(R1,approach(D),10) :- hasdoor(R1,D).
+heuristics(R1,gothrough(D),10) :- hasdoor(R1,D).
+
+
+#program step(k).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Actions
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+facing(D,k) :- approach(D,k-1).
+beside(D,k) :- approach(D,k-1).
+at(R2,k) :- approach(D,k-1), at(R1,k-1), hasdoor(R2,D), acc(R1,R2).
+:- approach(D,k-1), at(R1,k-1), dooracc(R3,D,R2), not acc(R1,R3), not acc(R1,R2).
+
+at(R2,k) :- gothrough(D,k-1), at(R1,k-1), dooracc(R1,D,R2).
+-facing(D,k) :- gothrough(D,k-1), facing(D,k-1).
+:- gothrough(D,k-1), not facing(D,k-1).
+:- gothrough(D,k-1), not open(D,k-1).
+%cannot go through if the person in office is not available
+:- gothrough(D,k-1), at(R1,k-1), dooracc(R1,D,R2), hasoffice(P,R2), not accessgranted(D,k-1).
+
+open(D,k) :- opendoor(D,k-1).
+facing(D,k) :- opendoor(D,k-1).
+beside(D,k) :- opendoor(D,k-1).
+:- opendoor(D,k-1), badDoor(D).
+:- opendoor(D,k-1), not facing(D,k-1).
+:- opendoor(D,k-1), closedofficedoor(D,k-1).
+:- opendoor(D,k-1), open(D,k-1).
+
+facing(O,k) :- goto(O,k-1).
+beside(O,k) :- goto(O,k-1).
+at(R,k) :- goto(O,k-1), inside(O,R).
+:- goto(O,k-1), inside(O,R1), at(R2,k-1), not acc(R1,R2).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Change on plan quality
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+cost(Q+R,k) :- approach(D,k-1), at(R1,k-1), cost(Q,k-1), ro(R1,approach(D),R), R!=0.
+% if there is learned value, use it
+
+cost(Q+R,k) :- approach(D,k-1), at(R1,k-1), cost(Q,k-1), not ro(R1,approach(D),R), heuristics(R1,approach(D),R), R!=0.
+% if there is no learned value, use heuristics obtained from navigation
+
+cost(Q+R,k) :- gothrough(D,k-1), at(R1,k-1), dooracc(R1,D,R2), cost(Q,k-1), ro(R1,gothrough(D),R), R!=0.
+cost(Q+R,k) :- gothrough(D,k-1), at(R1,k-1), dooracc(R1,D,R2), cost(Q,k-1), not ro(R1,gothrough(D),R), heuristics(R1,gothrough(D),R), R!=0.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Static laws
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%you can't be at two places at the some time
+-at(L2,k):- at(L1,k), room(L2), L1 != L2.
+
+%you can only be beside a door at any given time (if you delete this,
+%the observations must also return -beside which doesn't happen at the moment.
+-beside(D,k) :- beside(D,k-1), at(R,k), not hasdoor(R,D), door(D).
+-beside(O2,k):- beside(O1,k), beside(O2,k-1), O1 != O2.
+-beside(O,k) :- beside(O,k-1), at(R,k), not inside(O,R), object(O).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Inertia
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% cost is inertial
+{cost(R,k)} :- cost(R,k-1).
+:- #count{Q:cost(Q,k)}!=1.
+
+%at is inertial
+at(L,k) :- at(L,k-1), not -at(L,k).
+%facing is inertial
+facing(D,k) :- facing(D,k-1), not -facing(D,k).
+-facing(D,k) :- -facing(D,k-1), not facing(D,k).
+%you can be facing only one door at a time
+-facing(O2,k):- facing(O1,k), facing(O2,k-1), O1 != O2.
+% open is inertial
+open(D,k) :- open(D,k-1), not -open(D,k).
+-open(D,k) :- -open(D,k-1), not open(D,k), not -knowclosed(D,k).
+% beside is inertial
+beside(D,k) :- beside(D,k-1), not -beside(D,k).
+
+
+#show at/2.
+#show open/2.
+#show -open/2.
+#show facing/2.
+#show beside/2.
